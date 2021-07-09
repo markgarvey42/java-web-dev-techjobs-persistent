566df59b9596c3d07587c6efc4ad214ae3424238
Mark <Markgarvey42@gmail.com>
2021-07-08 19:49:45 -0500

    Finished Assignment #4

--- queries.sql
+++ queries.sql
@@ -9,8 +9,17 @@
 
     SELECT name
     FROM employer
-    WHERE location LIKE "St. Louis City"
+    WHERE location LIKE "St. Louis City";
 
 ## Part 3: Test it with SQL
 
+    DROP TABLE job;
+
 ## Part 4: Test it with SQL
+
+    SELECT skill.name, skill.description
+    FROM skill
+    LEFT JOIN job_skills
+    ON skill.id = job_skills.skills_id
+    WHERE skills_id IS NOT NULL
+    ORDER BY skill.name asc;
